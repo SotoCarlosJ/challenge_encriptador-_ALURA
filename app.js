b463dec86f3f789d514c5ff188a8c641ad2e180a
Carlos Soto <carloss.empire@gmail.com>
2024-08-07 15:42:22 -0300

    Agregando funciones para encriptar, desencriptar y copiar al portapapeles

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
-/* Las "llaves" de encriptación que utilizaremos son las siguientes:
-La letra "e" es convertida para "enter"
-La letra "i" es convertida para "imes"
-La letra "a" es convertida para "ai"
-La letra "o" es convertida para "ober"
-La letra "u" es convertida para "ufat"
-*/
+const message = document.getElementById('message');
+const result = document.getElementById('result');
+const btnCopy = document.getElementById('copy');
+const noMessage = document.getElementById('noMessage');
+
+const encrypt = (param) => {
+    const encryptionCode = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    let stringEncrypted = param.toLowerCase();
+    for (let i = 0; i < encryptionCode.length; i++) {
+        if (stringEncrypted.includes(encryptionCode[i][0])) {
+            stringEncrypted = stringEncrypted.replaceAll(encryptionCode[i][0], encryptionCode[i][1]);
+        }
+    }
+    return stringEncrypted;
+};
+
+const decrypt = (param) => {
+    const encryptionCode = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    let stringDencrypted = param.toLowerCase();
+    for (let i = 0; i < encryptionCode.length; i++) {
+        if (stringDencrypted.includes(encryptionCode[i][1])) {
+            stringDencrypted = stringDencrypted.replaceAll(encryptionCode[i][1], encryptionCode[i][0]);
+        }
+    }
+    return stringDencrypted;
+};
+
+const btnEncrypt = () => {
+    const messageEncrypted = encrypt(message.value);
+    message.value = "";
+    result.style.backgroundImage = "none";
+    noMessage.style.display = "none";
+    btnCopy.style.display = "block";
+    result.value = messageEncrypted
+};
+
+const btnDecrypt = () => {
+    const messageEncrypted = decrypt(message.value);
+    message.value = "";
+    result.value = messageEncrypted
+};
+
+const copy = () => navigator.clipboard.writeText(result.value);
