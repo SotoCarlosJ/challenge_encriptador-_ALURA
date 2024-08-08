4ecc1d9ccf6e68c3b3da8da3a796395c56e52272
Carlos Soto <carloss.empire@gmail.com>
2024-08-08 19:19:32 -0300

    Agregando una validacion para verificar si se ingresaron mayusculas o acentos

--- app.js
+++ app.js
@@ -2,36 +2,50 @@ const message = document.getElementById('message');
 const result = document.getElementById('result');
 const btnCopy = document.getElementById('copy');
 const noMessage = document.getElementById('noMessage');
+const errorMessage = document.getElementById('errorMessage');
+
+const encryptionCode = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
 
 const encrypt = (param) => {
-    const encryptionCode = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    let stringEncrypted = param.toLowerCase();
+    let stringEncrypted = "";
     for (let i = 0; i < encryptionCode.length; i++) {
-        if (stringEncrypted.includes(encryptionCode[i][0])) {
-            stringEncrypted = stringEncrypted.replaceAll(encryptionCode[i][0], encryptionCode[i][1]);
+        if (param.includes(encryptionCode[i][0])) {
+            stringEncrypted = param.replaceAll(encryptionCode[i][0], encryptionCode[i][1]);
         }
     }
     return stringEncrypted;
 };
 
 const decrypt = (param) => {
-    const encryptionCode = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    let stringDencrypted = param.toLowerCase();
+    let stringDencrypted = "";
     for (let i = 0; i < encryptionCode.length; i++) {
-        if (stringDencrypted.includes(encryptionCode[i][1])) {
-            stringDencrypted = stringDencrypted.replaceAll(encryptionCode[i][1], encryptionCode[i][0]);
+        if (param.includes(encryptionCode[i][1])) {
+            stringDencrypted = param.replaceAll(encryptionCode[i][1], encryptionCode[i][0]);
         }
     }
     return stringDencrypted;
 };
 
+const validateEntry = (string) => {
+    const accentRegex = /[áéíóúüÁÉÍÓÚÜàèìòùÀÈÌÒÙãõñçÃÕÑÇ]/;
+    const uppercaseRegex = /[A-Z]/;
+    if(accentRegex.test(string) || uppercaseRegex.test(string)) {
+        noMessage.textContent = "";
+        errorMessage.style.display = "block"
+        return false;
+    } else return true;
+};
+
 const btnEncrypt = () => {
-    const messageEncrypted = encrypt(message.value);
-    message.value = "";
-    result.style.backgroundImage = "none";
-    noMessage.style.display = "none";
-    btnCopy.style.display = "block";
-    result.value = messageEncrypted
+    if(validateEntry(message.value)) {
+        const messageEncrypted = encrypt(message.value);
+        message.value = "";
+        result.style.backgroundImage = "none";
+        noMessage.style.display = "none";
+        errorMessage.style.display = "none"
+        btnCopy.style.display = "block";
+        result.value = messageEncrypted
+    }
 };
 
 const btnDecrypt = () => {
@@ -40,4 +54,8 @@ const btnDecrypt = () => {
     result.value = messageEncrypted
 };
 
-const copy = () => navigator.clipboard.writeText(result.value);
+const copy = () => {
+    navigator.clipboard.writeText(result.value)
+        .then(() => alert('Copied to clipboard!'))
+        .catch(err => console.error('Failed to copy: ', err));
+};
